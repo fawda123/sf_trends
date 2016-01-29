3c06c2cb3abefaf20e6a3ac8e6b1484b7945beeb
fawda123 <mbafs2012@gmail.com>
2016-01-29 11:12:43 -0600

    started looking at nut data for active stations

---
+++ R/dat_proc.R
@@ -0,0 +1,36 @@
+library(dplyr)
+library(tidyr)
+library(readxl)
+library(ggplot2)
+
+load(file = 'data/dwr_wq.RData')
+
+statmeta <- read_excel('ignore/Discrete_Stations.xls')
+curr <- filter(statmeta, Historic_or_Current == 'Current') %>% 
+  .$Site_Code %>% 
+  unique
+hist <- filter(statmeta, Historic_or_Current == 'Historic') %>% 
+  .$Site_Code %>% 
+  unique
+
+### Combine C10 with C10A, C3 with C3A, P12 with P12A, P10 with P10A, MD10 with MD10A, MD7 with MD7A
+# the following three have to be added manually to curr because they combined in original processing script
+curr <- c('C10', 'C3', 'MD10')  
+
+# add lat/long from meta
+# only get current stations
+# remove EZ2/EZ6 (no lat/long data, TN record only back to 2010)
+# remove NZ stations, no nitrogen data
+# select ammonia, nitrate, dissolved inorganic nitrogen, total nitrogen (all as mg/L), and salinity (psu probably)
+dat <- select(statmeta, Site_Code, Latitude, Longitude) %>% 
+  na.omit %>% 
+  right_join(., dwr_wq, by = c('Site_Code' = 'Station')) %>% 
+  filter(Site_Code %in% curr) %>% 
+  filter(!Site_Code %in% c('EZ2', 'EZ6', 'NZ002', 'NZ004', 'NZ032', 'NZ325', 'NZS42')) %>% 
+  select(Site_Code, Date, Latitude, Longitude, nh, no23, din, tn, sal)
+
+toplo <- gather(dat, 'var', 'val', -Site_Code, -Date, -Latitude, -Longitude)
+  
+ggplot(toplo, aes(x = Date, y = val)) +
+  geom_line() +
+  facet_grid(var~Site_Code, scales = 'free_y')
