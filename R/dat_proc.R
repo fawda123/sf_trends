5aa8b8485fd7b22e7218694a97714fc51913213c
fawda123 <mbafs2012@gmail.com>
2016-06-23 13:00:13 -0500

    mods_nolag_mean added

--- R/dat_proc.R
+++ R/dat_proc.R
@@ -729,6 +729,139 @@ save(mods_nolag, file = 'sf_trends/mods_nolag.RData', compress = 'xz')
 save(mods_nolag, file = 'M:/docs/manuscripts/sftrends_manu/data/mods_nolag.RData', compress = 'xz')
 
 ######
+# create mean models using unlagged variables (for Sarah)
+
+rm(list = ls())
+
+cl <- makeCluster(7)
+registerDoParallel(cl)
+
+data(mods_nolag)
+
+mods_nolag_mean <- select(mods_nolag, -mod)
+
+# resvar label lookup
+lablk <- list(
+  shrt = c('din', 'nh', 'no23'),
+  lngs = c(
+    expression(paste('ln-dissolved inorganic nitrogen (mg ', L^-1, ')')),
+    expression(paste('ln-ammonium (mg ', L^-1, ')')),
+    expression(paste('ln-nitrite/nitrate (mg ', L^-1, ')'))
+    )
+  )
+
+# flovar label lookup
+stalk <- list(
+  shrt = c('sjr', 'sac', 'sal'),
+  lngs = c('San Joaquin', 'Sacramento', 'Salinity')
+  )
+
+strt <- Sys.time()
+
+# iterate through stations, res vars to model
+# get predictions from obs time series of salinity or flow
+mods_out <- foreach(i = 1:nrow(mods_nolag_mean)) %dopar% {
+  
+  data(flow_dat)
+  data(delt_dat)
+  
+  library(dplyr)
+  library(WRTDStidal)
+  
+  sink('C:/Users/mbeck/Desktop/log.txt')
+  cat(i, 'of', nrow(mods_nolag_mean), '\n')
+  print(Sys.time()-strt)
+  sink()
+  
+  # data, respons variable label
+  dat <- mods_nolag_mean[i, ]$data[[1]]
+  resvar <- mods_nolag_mean[i, ]$resvar
+  flovar <- mods_nolag_mean[i, ]$flovar
+  sta <- mods_nolag_mean[i, ]$Site_Code
+  reslab <- with(lablk, lngs[shrt == resvar])
+  flolab <- with(stalk, shrt[lngs == flovar])
+  
+  # prep data as tidal object
+  tomod <- select(dat, Date, resval, flolag, lim) %>% 
+    rename(
+      res = resval, 
+      flo = flolag
+    ) %>% 
+    data.frame %>% 
+    tidalmean(., 
+      reslab = reslab, 
+      flolab = expression(paste('ln-flow (standardized)'))
+    )
+
+  # get flo or salinity variable to predict 
+  if(flolab == 'sal'){
+    
+    topred <- filter(delt_dat, Site_Code == sta) %>% 
+      mutate(flo = log(1 + sal)) %>% # salinity is only variable with zeroes
+      rename(date = Date) %>% 
+      select(date, flo) %>% 
+      filter(date >= min(tomod$date) & date <= max(tomod$date)) %>% 
+      na.omit %>% 
+      data.frame
+    
+  } else {
+    
+    topred <- filter(flow_dat, station == flolab) %>% 
+      mutate(flo = log(q)) %>% 
+      rename(date = Date) %>% 
+      select(date, flo) %>% 
+      filter(date >= min(tomod$date) & date <= max(tomod$date)) %>% 
+      na.omit %>% 
+      data.frame
+    
+    # if D19, remove gap in time series with no data
+    if(sta == 'D19'){
+      
+      dts <- as.Date(c('1995-12-01', '2004-05-01')) # looked at orig record for this
+      
+      torm <- with(topred, date > dts[1] & date < dts[2])
+      topred$flo[torm] <- NA
+      
+    }
+    
+  }
+  
+  # create model and exit
+  mod <- wrtds(tomod, wins = list(0.5, 10, 0.5), flo_div = 30, min_obs = 150)
+  
+  # get predictions, norms from obs flow data
+  out <- mod %>% 
+    respred(dat_pred = topred) %>% 
+    resnorm
+
+  # assign to unique object, save in case of fuckery
+  outnm <- paste0(sta, '_', resvar)
+  assign(outnm, out)
+  save(list = outnm, file = paste0('data/', outnm, '.RData'), compress = 'xz')
+  
+  # out for list
+  out
+  
+}
+
+# import each file, add to nested mods_nolag_mean dataframe
+fls <- list.files('data', pattern = '^C3|^C10|^P8|^D6|^D4|^D7|^D19|^D26|^D28', full.names = T)
+dat <- lapply(fls, load, .GlobalEnv)
+names(dat) <- unlist(dat)
+dat <- lapply(dat, get)
+
+mods_nolag_mean <- unite(mods_nolag_mean, 'tmp', Site_Code, resvar, remove = F) %>% 
+  mutate(mod = dat[match(tmp, names(dat))]) %>% 
+  select(-tmp)
+
+# remove the individual files
+file.remove(fls)
+
+# save output, also to shiny app folder
+save(mods_nolag_mean, file = 'data/mods_nolag_mean.RData', compress = 'xz')
+save(mods_nolag_mean, file = 'M:/docs/manuscripts/sftrends_manu/data/mods_nolag_mean.RData', compress = 'xz')
+
+######
 # evaluate model fit for middle delta stations with three different flow variables
 
 rm(list = ls())
